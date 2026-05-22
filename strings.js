697c4d09db8186b53a9b4423497269dbb5116c55
Макарова <ovppov29@gmail.com>
2026-05-22 07:53:48 +0000

    tyntyn

--- strings.js
+++ strings.js
@@ -9,3 +9,16 @@ const group = "21-ИСР";
 const Score = "4.75";
 console.log(`Студент ${surname} ${name}, группа ${group}, средний балл: ${Score}.`);
 
+const date = "2025-03-15";
+const dateparts = date.split('-');
+console.log(`${dateparts[2]}.${dateparts[1]}.${dateparts[0]}`)
+
+const developer = "Node.js разработчик";
+const startwithnode = developer.startsWith("Node");
+const endwithdeveloper = developer.endsWith("разработчик");
+console.log(startwithnode);
+console.log(endwithdeveloper)
+
+const numbers = 42;
+console.log(numbers.toString().padStart(8,'0'));
+
